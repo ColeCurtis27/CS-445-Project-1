5e7aac321d43eb9258dc2af384c29536c999bf92
Cole Curtis <curtisco2004@gmail.com>
2026-03-20 16:17:34 -0400

    Update vite.config.js

--- cs445project1/vite.config.js
+++ cs445project1/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://ColeCurtis27.github.io/CS-445-Project-1/"
+  base: "https://<ColeCurtis27>.github.io/<CS-445-Project-1>/"
 })
